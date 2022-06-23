695e6666f8583dce3507831b7b710596b9dd82d2
TayebBou <tayebbousfiha72@gmail.com>
2022-06-23 15:00:19 +0200

    add animations and fetch alarms by date with calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import styles from './App.module.css'
 import data from './server/alarms.json'
 import { IAlarm } from './types/alarm.model'
 import clock from './assets/images/clock.png'
 import Alarm from './components/statelessComponents/Alarm'
+import { Calendar } from 'primereact/calendar';
+
 
 const App: FC = () => {
-  const alarms: IAlarm[] = data
+  const [date, setDate] = useState<Date | Date[] | undefined>(undefined);
+  const [alarms, setAlarms] = useState<IAlarm[]>(data);
+
+  useEffect(() => {
+    const dateChoosed = date?.toLocaleString().split(',')[0];
+    if(date) {
+      setAlarms(data.filter((a: IAlarm) => dateChoosed === new Date(parseInt(a.alarm_time)*1000).toLocaleDateString()))
+    } else {
+      setAlarms(data.filter((a: IAlarm) => new Date().toLocaleDateString() === new Date(parseInt(a.alarm_time)*1000).toLocaleDateString()))
+    }
+  },[date])
 
   return (
     <>
       <div className={styles.wallpaper}></div>
       <h1 className={styles.h1}>Daily Alarms</h1>
       <img className={styles.clock} src={clock} alt="clock" />
-      {alarms.map((alarm: IAlarm) => (
-        <Alarm alarm={alarm} />
-      ))}
+      <Calendar className={styles.calendar} value={date} onChange={(e) => setDate(e.value)} inline showWeek />
+      {alarms.map((alarm: IAlarm) => <Alarm alarm={alarm} key={alarm._id} />)}
     </>
   )
 }
